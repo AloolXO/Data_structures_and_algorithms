c4933645a3582b6a8fc80975380bbbda8e3485bc
AloolXO <al.university.edu@gmail.com>
2021-03-14 21:23:01 +1000

    Ex3

---
+++ Sergey/lab_4/Ex3/cmake-build-debug/CMakeFiles/Ex3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex3.dir/main.cpp.o"
+  "Ex3.exe"
+  "Ex3.pdb"
+  "libEx3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
