874c10ae9e8fc0660ee3c4f0c9a09646277206d2
AloolXO <al.university.edu@gmail.com>
2021-03-14 21:12:27 +1000

    Correction

--- Sergey/lab_4/Ex2/main.cpp
+++ Sergey/lab_4/Ex2/main.cpp
@@ -4,4 +4,16 @@ using namespace std;
 
 int main() {
 
+    //double a = 176.24;
+    auto *x = new double (176.24);
+    auto *y = new double (176.24);
+
+    cout << "Значение указателя x = " << *x << endl;
+    cout << "Адрес указателя x: " << x << endl;
+    cout << "Значение указателя y = " << *y+26.84 << endl;
+    cout << "Адрес указателя y: " << y  << endl;
+
+    delete (y);
+    delete (x);
+
 }
