f8b3cd3062c58d04452a71d3cda8525d517e649a
AloolXO <al.university.edu@gmail.com>
2021-03-24 23:31:40 +1000

    lab_8, var 2

---
+++ Sergey/lab_8/cmake-build-debug/CMakeFiles/lab_8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_8.dir/main.cpp.o"
+  "lab_8.exe"
+  "lab_8.pdb"
+  "liblab_8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
