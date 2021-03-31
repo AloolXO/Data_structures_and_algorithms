18f6db3b2210fcf6d2324da50e5b466ff7e8d676
AloolXO <al.university.edu@gmail.com>
2021-03-31 23:56:33 +1000

    Update 1.2

--- Artem/lab_3/var_23/main.cpp
+++ Artem/lab_3/var_23/main.cpp
@@ -3,16 +3,6 @@
 
 using namespace std;
 
-long double fact(int n)
-{
-    if(n < 0) // если пользователь ввел отрицательное число
-        return 0; // возвращаем ноль
-    if (n == 0) // если пользователь ввел ноль,
-        return 1; // возвращаем факториал от нуля - не удивляетесь, но это 1 =)
-    else // Во всех остальных случаях
-        return n * fact(n - 1); // делаем рекурсию.
-}
-
 int main() {
 
     int n; long double epsilon, y, x, p;
