937f648cacac52def88c483611aa38c4e718e769
AloolXO <al.university.edu@gmail.com>
2021-03-31 23:04:19 +1000

    var-23, lab_8

---
+++ Artem/lab_8/var_23/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int sum_f(int** arr, int n){
+    int s=0;
+    for (int i = 0; i < n;i++){
+        for (int j = 0;j<n;j++)
+            if ((j<(n-1)-i)||(j==(n-1)-i))
+                s += arr[i][j];
+    }
+    return s;
+}
+
+void print (int** arr, int n, int sum){
+    cout << "\t" << " ---- Массив ---- ";
+    for (int i = 0; i < n;i++) {
+        cout << "\n";
+        for (int j = 0; j < n; j++)
+            cout << setw(5) << arr[i][j];
+    }
+    cout << "\n" << "\n" << "Результат вычисления: " << sum << "\n";
+}
+
+void del (int** arr, int n){
+    for (int i = 0; i < n;i++) {
+        for (int j = 0; j < n; j++)
+            delete arr[i];
+    }
+    delete [] arr;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n, sum, **arr, count = 1;
+    cout << "Введите размерность массива: ";
+    cin >> n;
+    arr = new int *[n];
+
+    //Создание массива и заполнение 1 и 0
+    for (int i = 0; i < n;i++){
+        arr[i] = new int [n];
+        for (int j = 0;j<n;j++)
+            if ((j<(n-1)-i)||(j==(n-1)-i))
+                arr[i][j]= 1;
+            else
+                arr[i][j]= 0;
+    }
+
+    sum = sum_f(arr, n);
+
+    //Вывод массива и результата
+
+    print(arr, n, sum);
+
+    //Создание массива и заполнение цифрами
+    for (int i = 0; i < n;i++){
+        for (int j = 0;j<n;j++) {
+            arr[i][j] = count;
+            count++;
+        }
+
+    }
+
+    sum = sum_f(arr, n);
+
+    //Вывод массива и результата
+
+    print(arr, n, sum);
+
+    //Удаление мссива
+
+    del (arr, n);
+}
