25fae55f6a6d1fbf087d53e4e352b484e4a61b7a
AloolXO <al.university.edu@gmail.com>
2021-03-31 15:51:58 +1000

    Lab_7 whis struct

---
+++ lab_9/cmake-build-debug/CMakeFiles/lab_9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_9.dir/main.cpp.o"
+  "lab_9.exe"
+  "lab_9.pdb"
+  "liblab_9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab_9/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+struct  Turnover {
+    int index[12];
+    string name[12];
+    float count[12];
+};
+
+void print(const Turnover& tur, int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << tur.index[i] << "\t" << tur.name[i] << "\t" << tur.count[i] << endl;
+}
+
+void bubble(Turnover& t, int n)
+{
+    for (int i = 1; i < n; ++i)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            if (t.count[j] < t.count[j + 1])
+            {
+                float tmp = t.count[j];
+                t.count[j] = t.count[j + 1];
+                t.count[j + 1] = tmp;
+
+                string tmp1 = t.name[j];
+                t.name[j] = t.name[j + 1];
+                t.name[j + 1] = tmp1;
+
+                int tmp2 = t.index[j];
+                t.index[j] = t.index[j + 1];
+                t.index[j + 1] = tmp2;
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+   struct Turnover tr = {{1,2,3,4,5,6,7,8,9,10,11,12},
+                           { "Янв","Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" },
+                           { 102.6, 98.4, 102.3, 101.8, 96.5, 96.7, 96.8, 99.0, 105.2, 104.0, 105.3, 103.4 }};
+
+    int n =12;
+
+    cout << "--- Массив до сортировки ---" << endl;
+    print(tr, n);
+
+    bubble(tr, n);
+
+    cout << "--- Массив после сортировки ---" << endl;
+    print(tr, n);
+
+    cout << "\nНомер и название месяца, показатель товарооборота в котором находится после сортировки на предпоследнем месте: " << tr.index[10] << " " << tr.name[10] << endl;
+}
