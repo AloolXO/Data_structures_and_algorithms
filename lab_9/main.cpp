fa99fd55547c7c5efd4dd37499ffdb9230009954
AloolXO <al.university.edu@gmail.com>
2021-04-02 15:05:33 +1000

    Update 1.1

--- lab_9/main.cpp
+++ lab_9/main.cpp
@@ -3,36 +3,37 @@
 using namespace std;
 
 struct  Turnover {
-    int index[12];
-    string name[12];
-    float count[12];
+    int index;
+    string name;
+    float count;
 };
 
-void print(const Turnover& tur, int n)
+void print(const Turnover tur[], int n)
 {
     for (int i = 0; i < n; ++i)
-        cout << tur.index[i] << "\t" << tur.name[i] << "\t" << tur.count[i] << endl;
+        cout << tur[i].index << "\t" << tur[i].name << "\t" << tur[i].count << endl;
 }
 
-void bubble(Turnover& t, int n)
+
+void bubble(Turnover t[], int n)
 {
     for (int i = 1; i < n; ++i)
     {
         for (int j = 0; j < n-i; j++)
         {
-            if (t.count[j] < t.count[j + 1])
+            if (t[j].count < t[j + 1].count)
             {
-                float tmp = t.count[j];
-                t.count[j] = t.count[j + 1];
-                t.count[j + 1] = tmp;
+                float tmp = t[j].count;
+                t[j].count = t[j + 1].count;
+                t[j + 1].count = tmp;
 
-                string tmp1 = t.name[j];
-                t.name[j] = t.name[j + 1];
-                t.name[j + 1] = tmp1;
+                string tmp1 = t[j].name;
+                t[j].name = t[j + 1].name;
+                t[j + 1].name = tmp1;
 
-                int tmp2 = t.index[j];
-                t.index[j] = t.index[j + 1];
-                t.index[j + 1] = tmp2;
+                int tmp2 = t[j].index;
+                t[j].index = t[j + 1].index;
+                t[j + 1].index = tmp2;
             }
         }
     }
@@ -41,11 +42,18 @@ void bubble(Turnover& t, int n)
 int main()
 {
     setlocale(LC_ALL, "Russian");
-   struct Turnover year1 = {{1,2,3,4,5,6,7,8,9,10,11,12},
-                            { "Янв","Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" },
-                            { 102.6, 98.4, 102.3, 101.8, 96.5, 96.7, 96.8, 99.0, 105.2, 104.0, 105.3, 103.4 }};
+    int index[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    string name[] = { "Янв","Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" };
+    float count[] = { 102.6, 98.4, 102.3, 101.8, 96.5, 96.7, 96.8, 99.0, 105.2, 104.0, 105.3, 103.4 };
+   struct Turnover year1[12] = {};
 
     int n =12;
+
+    for (int i = 0; i < n; ++i){
+        year1[i].index = index[i];
+        year1[i].name = name[i];
+        year1[i].count = count[i];
+    }
 
     cout << "--- Массив до сортировки ---" << endl;
     print(year1, n);
@@ -55,5 +63,5 @@ int main()
     cout << "--- Массив после сортировки ---" << endl;
     print(year1, n);
 
-    cout << "\nНомер и название месяца, показатель товарооборота в котором находится после сортировки на предпоследнем месте: " << year1.index[10] << " " << year1.name[10] << endl;
+    cout << "\nНомер и название месяца, показатель товарооборота в котором находится после сортировки на предпоследнем месте: " << year1[10].index << " " << year1[10].name << endl;
 }
