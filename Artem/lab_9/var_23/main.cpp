9cf56d37e0181c2a4dbca700a9de38a1924fe359
AloolXO <al.university.edu@gmail.com>
2021-04-02 17:30:11 +1000

    Update 1.1

--- Artem/lab_9/var_23/main.cpp
+++ Artem/lab_9/var_23/main.cpp
@@ -3,57 +3,50 @@
 using namespace std;
 
 struct  Turnover {
-    int number[11];
-    string name[11];
-    float count[11];
+    int number;
+    string name;
+    float count;
 };
 
-void print(const Turnover& tur, int n)
+void print(const Turnover tur[], int n)
 {
     for (int i = 0; i < n; ++i)
-        cout << tur.number[i] << "\t" << tur.name[i] << "\t" << tur.count[i] << endl;
+        cout << tur[i].number << "\t" << tur[i].name << "\t" << tur[i].count << endl;
 }
 
-void bubble(Turnover& t, int n)
+void bubble(Turnover a[], int n)
 {
-    for (int i = 1; i < n; ++i)
-    {
-        for (int j = 0; j < n-i; j++)
-        {
-            if (t.count[j] < t.count[j + 1])
-            {
-                float tmp = t.count[j];
-                t.count[j] = t.count[j + 1];
-                t.count[j + 1] = tmp;
-
-                string tmp1 = t.name[j];
-                t.name[j] = t.name[j + 1];
-                t.name[j + 1] = tmp1;
-
-                int tmp2 = t.number[j];
-                t.number[j] = t.number[j + 1];
-                t.number[j + 1] = tmp2;
-            }
+    for(int i=1;i<n;i++)
+        for(int j=i;j>0 && a[j-1].count>a[j].count;j--) { // пока j>0 и элемент j-1 > j, x-массив int
+            swap(a[j - 1].count, a[j].count);
+            swap(a[j - 1].number, a[j].number);
+            swap(a[j - 1].name, a[j].name);
         }
-    }
 }
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-   struct Turnover year1 = {{1,2,3,4,5,6,7,8,9,10,11},
-                            { "Топаз", "Лазер", "Свет", "Факел", "Луч", "Искра", "Квант", "Лев", "Утёс", "Агат", "Озон" },
-                            { -353.9, 455.8, 538.5, 437.8, 1553.7, 647.3, 515,2-174.4, 124.0, 647.7, 496.6}};
+    int number[] = {1,2,3,4,5,6,7,8,9,10,11};
+    string name[] =  { "Топаз", "Лазер", "Свет", "Факел", "Луч", "Искра", "Квант", "Лев", "Утёс", "Агат", "Озон" };
+    float count[] = { -353.9, 455.8, 538.5, 437.8, 1553.7, 647.3, 515,2-174.4, 124.0, 647.7, 496.6};
+   struct Turnover product[11] = {};
 
-    int n =12;
+    int n =11;
 
-    cout << "--- Массив до сортировки ---" << endl;
-    print(year1, n);
+    for (int i = 0; i < n; ++i) {
+        product[i].number = number[i];
+        product[i].name = name[i];
+        product[i].count = count[i];
+    }
 
-    bubble(year1, n);
+        cout << "--- Массив до сортировки ---" << endl;
+    print(product, n);
+
+    bubble(product, n);
 
     cout << "--- Массив после сортировки ---" << endl;
-    print(year1, n);
+    print(product, n);
 
-    cout << "\nНомер и название месяца, показатель товарооборота в котором находится после сортировки на предпоследнем месте: " << year1.number[10] << " " << year1.name[10] << endl;
+    cout << "\nНомер и название месяца, показатель товарооборота в котором находится после сортировки на предпоследнем месте: " << product[9].number << " " << product[9].name << endl;
 }
